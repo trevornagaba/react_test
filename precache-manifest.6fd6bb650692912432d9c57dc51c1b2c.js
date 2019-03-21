e25dfe01d4e6e4380075a7c8a4e537be057530ed
Trevor Nagaba <trevornagaba@gmail.com>
2019-03-21 13:54:29 +0000

    Updates

---
+++ precache-manifest.6fd6bb650692912432d9c57dc51c1b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37c519b1b0bef28b0825",
+    "url": "/react_test/static/js/runtime~main.13abe56d.js"
+  },
+  {
+    "revision": "6631d71ffa2f1ba74025",
+    "url": "/react_test/static/js/main.a0324526.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react_test/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "6631d71ffa2f1ba74025",
+    "url": "/react_test/static/css/main.d357948d.chunk.css"
+  },
+  {
+    "revision": "6cb2a3401b8c841000824689f623caab",
+    "url": "/react_test/index.html"
+  }
+];
