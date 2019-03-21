7e7b6c8a71316b99d7ff9f4661049163a4348f8f
Trevor Nagaba <trevornagaba@gmail.com>
2019-03-21 14:00:55 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_test/precache-manifest.6fd6bb650692912432d9c57dc51c1b2c.js"
+  "/react_test/react_app/precache-manifest.97780d33df9cfd8c172e8df9ead877df.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react_test/index.html", {
+workbox.routing.registerNavigationRoute("/react_test/react_app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
